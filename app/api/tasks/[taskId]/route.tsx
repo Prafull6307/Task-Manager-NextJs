d621e0ba9e637cc321b1d7a3aec3108e54e667cc
Prafull6307 <prafullps6307@gmail.com>
2024-04-25 15:00:03 +0530

    thirrdd commit

--- app/api/tasks/[taskId]/route.tsx
+++ app/api/tasks/[taskId]/route.tsx
@@ -1,12 +1,22 @@
 import { Task } from "@/app/modules/task";
 import { NextRequest, NextResponse } from "next/server";
+import { JwtPayload } from "jsonwebtoken";
+
 
 export async function DELETE(request: NextRequest, { params }: { params: { taskId: string } }) {
   try {
-    const { taskId } = params;
+   
+    
+    // Parse request body to JSON
+    const body = await request.json();
+    
+    // Assuming data is obtained from somewhere else, such as a JWT token
+    const data = body.data as JwtPayload;
+
     await Task.deleteOne({
-      _id: taskId,
+      _id: data._id, // Type assertion here
     });
+
     return NextResponse.json({
       message: "Task Deleted",
       success: true
