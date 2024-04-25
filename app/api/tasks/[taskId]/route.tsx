3c7a06da440d14d288a72197fa5b8f30dc225f24
Prafull6307 <prafullps6307@gmail.com>
2024-04-25 14:36:37 +0530

    thirdd commit

--- app/api/tasks/[taskId]/route.tsx
+++ app/api/tasks/[taskId]/route.tsx
@@ -1,22 +1,21 @@
 import { Task } from "@/app/modules/task";
 import { NextRequest, NextResponse } from "next/server";
 
-
-
-
-
-export async function DELETE(request:NextRequest,{params}:{ params: { taskId: string } }){
-try {
-     const {taskId}=params;
-      await Task.deleteOne({
-          _id:taskId,
-      });
-      return NextResponse.json({
-          message:"Task Deleted",
-          success:true
-      })
-
-} catch (error) {
-     console.log("Error in deleting");
-}
+export async function DELETE(request: NextRequest, { params }: { params: { taskId: string } }) {
+  try {
+    const { taskId } = params;
+    await Task.deleteOne({
+      _id: taskId,
+    });
+    return NextResponse.json({
+      message: "Task Deleted",
+      success: true
+    });
+  } catch (error) {
+    console.log("Error in deleting", error);
+    return NextResponse.json({
+      message: "Error deleting task",
+      success: false
+    });
+  }
 }
